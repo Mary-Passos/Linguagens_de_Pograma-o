89164d2a0a5b051fd992511e0ee723fc904ecf10
Maria_passos <mariapassos84019@gmail.com>
2026-08-17 11:43:09 -0300

    Add files via upload

---
+++ PHP/sintaxe.php
@@ -0,0 +1,11 @@
+//cometário de uma linha
+#também comentário de uma linha
+
+/*
+cometário de ultipla linhas
+    muúltiplas mesmo. . .
+*/ 
+
+# Comandos de saída: Print e echo
+# variáveis : começam com cifrão($), não precisa declarar tipos. Regras básicas: não pode começar com número, naõ pode conter espaço.
+# Comandos: parte que tenho que aprende na prática. Regras não definidas e aprender sintaxe
